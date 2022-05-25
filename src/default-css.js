318781b3d0cbdcb6503c607a6cd5a3ad68ae44e4
UmamiAppearance <mail@umamiappearance.eu>
2022-05-25 15:33:35 +0200

    splitting files

---
+++ src/default-css.js
@@ -0,0 +1,71 @@
+const defaultCSS = `
+.html-console {
+    position: inherit;
+    display: block;
+    font-family: monospace;
+    font-size: inherit;
+    min-width: 100px;
+    min-height: 100px;
+    white-space: break-spaces;
+    overflow: auto;
+    margin: auto;
+    background-color: #fff;
+    color: black;
+    padding: 1px;
+}
+.html-console.default > .log {
+    border-color: rgba(157, 157, 157, 0.2);
+    border-width: 0 0 1pt 0;
+    border-style: solid;
+    padding: 2px 5px;
+}
+.html-console.default > .log:first-child {
+    border-width: 1pt 0;
+}
+.html-console.default > .warn {
+    background-color: rgb(248, 255, 147);
+}
+.html-console.default > .warn > span {
+    color: rgb(80, 80, 0) !important;
+}
+.html-console.default > .error {
+    background-color: rgb(236, 143, 143);
+}
+.html-console.default > .error > span {
+    color: rgb(100, 0, 0) !important;
+}
+.html-console.default > .log > .null {
+    color: rgb(127, 127, 127);
+}
+.html-console.default > .log > .number, .html-console.default > .log > .bigint, .html-console.default > .log > .object, .html-console.default > .log > .boolean {
+    color: rgb(50, 150, 60);
+}
+.html-console.default > .log > .array-string, .html-console.default > .log > .fn-args, .html-console.default > .log > .symbol {
+    color: rgb(255, 0, 255);
+}
+.html-console.default > .log > .function, .html-console.default > .log > .object {
+    color: rgb(40, 100, 250);
+}
+.html-console.default table {
+    width: 100%;
+    text-align: right;
+    border-spacing: 0;
+    border-collapse: collapse;
+    border: 2px #333;
+    border-style: solid none;
+}
+.html-console.default thead, .html-console.default th {
+    font-weight: 700;
+}
+.html-console.default thead > tr, .html-console.default tr:nth-child(even) {
+    background-color: rgba(200, 200, 220, 0.1);
+}
+.html-console.default th, .html-console.default td {
+    padding: 3px 0;
+    border: 1px solid rgba(157, 157, 157, 0.2);
+}
+`;
+
+export default defaultCSS;
+
+
