ca0f324f8a0eaa2636f123d6a6ca4b618fbc1797
UmamiAppearance <mail@umamiappearance.eu>
2022-05-31 14:15:38 +0200

    work in progress

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import { terser } from "rollup-plugin-terser";
+import { string } from "rollup-plugin-string";
+import stripCode from "rollup-plugin-strip-code";
 
 const exports = {
     input: "./src/contodo.js",
     output: [ 
-        {   
+        /*{   
             format: "iife",
             name: "contodo",
             file: "./dist/contodo.iife.js"
@@ -13,7 +15,7 @@ const exports = {
             name: "contodo",
             file: "./dist/contodo.iife.min.js",
             plugins: [terser()]
-        },
+        },*/
         {   
             format: "es",
             name: "contodo",
@@ -25,6 +27,16 @@ const exports = {
             file: "./dist/contodo.esm.min.js",
             plugins: [terser()]
         },
+    ],
+    plugins: [
+        stripCode({
+            pattern: "import defaultCSS from \"./default-css.js\";",
+
+        }),
+        string({
+            // Required to be specified
+            include: "**/*.css",
+        })
     ]
 };
 
