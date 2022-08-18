a481bc37a73d118f3c06424c653f92efcd3d5c3d
UmamiAppearance <mail@umamiappearance.eu>
2022-08-18 12:57:41 +0200

    v0.2.2

--- test/test.js
+++ test/test.js
@@ -1,19 +1,17 @@
-import NoBroCote from "no-bro-cote";
-
-const test = new NoBroCote(import.meta.url);
+import { test } from "no-bro-cote";
 
 test.addImport("import ConTodo from './dist/contodo.esm.js'");
 
 test.makeUnit(
-    "logTest",
-    "test",
-    () => {
-        const logInput = "test";
+    "log Test",
+    "test output",
+    (logInput) => {
         // eslint-disable-next-line no-undef
         const contodo = new ConTodo();
         console.log(logInput);
         return contodo.mainElem.childNodes[0].textContent;
-    }
+    },
+    "test output"
 );
 
 test.init();
