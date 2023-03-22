b3dfc1608a3eb27ee1f1082316030fef037e16e6
UmamiAppearance <mail@umamiappearance.eu>
2023-03-22 11:32:33 +0100

    v0.4.3

--- dist/contodo.esm.js
+++ dist/contodo.esm.js
@@ -28,7 +28,7 @@ var defaultCSS = ".contodo{position:inherit;display:block;font-family:monospace;
 /**
  * [contodo]{@link https://github.com/UmamiAppearance/contodo}
  *
- * @version 0.4.2
+ * @version 0.4.3
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
--- src/contodo.js
+++ src/contodo.js
@@ -1,7 +1,7 @@
 /**
  * [contodo]{@link https://github.com/UmamiAppearance/contodo}
  *
- * @version 0.4.2
+ * @version 0.4.3
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license MIT
  */
